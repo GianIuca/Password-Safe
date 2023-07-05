cb653fa944d97acc7e4290c6b2e5d2a0d6d55a01
Maurin Schucan <maurin@schucan.ch>
2023-07-05 16:52:45 +0200

    authentication

--- Backend/src/main/java/ch/bbw/backend/Controller.java
+++ Backend/src/main/java/ch/bbw/backend/Controller.java
@@ -1,12 +1,75 @@
 package ch.bbw.backend;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import ch.bbw.backend.JwtUserDetailsService;
+
+
+import ch.bbw.backend.JwtTokenUtil;
+import ch.bbw.backend.JwtRequest;
+import ch.bbw.backend.JwtResponse;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
 
 @RestController
+@CrossOrigin
 public class Controller {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private JwtUserDetailsService userDetailsService;
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String token = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+    }
+
+
+
+
+
+    /*
     @GetMapping("/login/{username}/{password}")
     public boolean login(@PathVariable String username, @PathVariable String password) {
         String usernameFile = "";
@@ -24,4 +87,45 @@ public class Controller {
 
         return list;
     }
+
+    @PostMapping("/add")
+    public void addPassword() {
+
+    }
+
+    @PutMapping("/change")
+    public void changeMaster() {
+
+    }
+*/
+    /*
+    //generateKey();
+    String content = "Name: Schucan\n" +
+            "Vorname: Maurin\n" +
+            "Klasse: 5IA20b";
+        try {
+        byte[] data = content.getBytes(StandardCharsets.UTF_8);//...data to encrypt
+        Cipher cipher = Cipher.getInstance("RSA");
+        //cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] bytesToSend = cipher.doFinal(data);
+
+        byte[] bytesReceived = bytesToSend;
+
+        Cipher cipherDecrypt = Cipher.getInstance("RSA");
+        //cipherDecrypt.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decrypted = cipherDecrypt.doFinal(bytesReceived);
+        System.out.println(new String((decrypted)));
+    } catch (
+    NoSuchPaddingException e) {
+        e.printStackTrace();
+    } catch (
+    IllegalBlockSizeException e) {
+        e.printStackTrace();
+    } catch (
+    NoSuchAlgorithmException e) {
+        e.printStackTrace();
+    } catch (
+    BadPaddingException e) {
+        e.printStackTrace();
+    }*/
 }
---
+++ Backend/src/main/java/ch/bbw/backend/WebSecurityConfig.java
@@ -0,0 +1,63 @@
+package ch.bbw.backend;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+
+    @Autowired
+    private UserDetailsService jwtUserDetailsService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        // configure AuthenticationManager so that it knows from where to load
+        // user for matching credentials
+        // Use BCryptPasswordEncoder
+        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        // We don't need CSRF for this example
+        httpSecurity.csrf((csrf) -> csrf.disable())
+                // dont authenticate this particular request
+                .authorizeRequests().requestMatchers("/authenticate").permitAll().
+                // all other requests need to be authenticated
+                        anyRequest().authenticated().and().
+                // make sure we use stateless session; session won't be used to
+                // store user's state.
+                        exceptionHandling((exceptionHandling) ->
+                        exceptionHandling
+                                .accessDeniedPage("/errors/access-denied")).sessionManagement((sessionManagement) ->
+                                sessionManagement
+                                        .sessionConcurrency((sessionConcurrency) ->
+                                                sessionConcurrency
+                                                        .maximumSessions(1)
+                                                        .expiredUrl("/login?expired")));
+
+        // Add a filter to validate the tokens with every request
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+}
